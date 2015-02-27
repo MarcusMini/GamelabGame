05e6acc58ca7aedd330b641f5fad30c5834cc1a9
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2015-02-27 19:15:31 +0100

    Update gemmeGUI.js

--- Game/Script/Common/gemmeGUI.js
+++ Game/Script/Common/gemmeGUI.js
@@ -1,13 +1,22 @@
 #pragma strict
 
 var timer : float;
+var animationFrame : int;
 var count : int;
 var customGuiStyle : GUIStyle;
 var i : int;
 
+var texture : Texture2D;
+
+// let's make a try... 
+// change the next time use Playerprefs or something else Charlotte oh yeah, keep going lalalalallalalalala, listening to music.
+var forcePos : int = 5;
+var vitessePos : int = 2;
+var visionPos : int = 4;
+var defensePos : int = 3;
+
 function Start () {
 	count = 1;
-	PlayerPrefs.DeleteAll();
 }
  
 function OnGUI(){
@@ -18,55 +27,83 @@ function OnGUI(){
 	
 	var bouton = ["defense", "force", "vision", "vitesse"];
 	var background = ["defenseBcg", "forceBcg", "visionBcg", "vitesseBcg"];
-	var molecule = ["forceMolecule", "", "", "", ""];
+	var molecule = ["forceMolecule", "visionMolecule", "vitesseMolecule", "defenseMolecule"];
 	// texte 8
 	var texte = ["Chacha", "Chachou", "Chama", "Chasi", "Charlotte", "Chakra", "Maya", "Gros bebe doudouce", "sss"];
+	var checkID = ["checkBleu", "check", "checkVert", "checkJaune"];
 	
 	// function which display the current element.
-	var DisplayText = function(tab : String[], c : int){
+	// put an id..
+	var DisplayText = function(tab : String[], c : int, ameliorationID : String, ptyState : int){
 			GUI.Label(Rect(Screen.width - 250,300,200,200), tab[c]);
-			if(GUI.Button(Rect(Screen.width - 200, 500, 120,70), Resources.Load("bouton") as Texture2D, customGuiStyle)){
-				Debug.Log("press "+c);
-				//PlayerPrefs.SetInt("steps", 3);
-				step(3);
+			if(ptyState <= c){
+				if(GUI.Button(Rect(Screen.width - 200, 500, 120,70), Resources.Load("bouton") as Texture2D, customGuiStyle)){
+					step(3);
+					triggerAnim();
+				}
 			}
 	};
 	
-	// loop function which will create the button it take 2 parameters the loop X and the loop Y. 
-	var createElement = function(loopX : int[], loopY : int[]){
+	
+	var createElement = function(loopX : int[], loopY : int[], level : int, idColor : int, ptyName : String){
 		for(var i : int = 0; i < loopX.length; i++){
-			if(GUI.Button(Rect(loopX[i], loopY[i], 25,25), Resources.Load("check") as Texture2D, customGuiStyle)){
-				Debug.Log("Je suis le numero" +i);
+				if(i < level){
+					texture = Resources.Load(checkID[idColor]) as Texture2D;
+				}
+				else{
+					texture = Resources.Load("checkBlanc") as Texture2D;
+				}
+			
+			if(GUI.Button(Rect(loopX[i], loopY[i], 17,17), texture, customGuiStyle)){
 				PlayerPrefs.SetInt("texteValue", i);
-				//PlayerPrefs.SetInt("steps", 2);
+				PlayerPrefs.SetString("ptyName", ptyName);
+				PlayerPrefs.SetInt("ptyState", level);
+				GUI.Label(Rect(Screen.width - 250,300,200,200), texte[i]);
 				step(2);
 			}
 		}
+		
+		// test
+		
 	};
 	
 	var defense = function(){
-		// ouvrir la fenetre de la defense	
-		GUI.BeginGroup(Rect(Screen.width - 300,height / 8,400,450), Resources.Load(background[0]) as Texture2D);
+		var positionDefenseX = [45,95,125,155,180,255];
+		var positionDefenseY = [130,130,105,160,133,133];
+		
+		GUI.BeginGroup(Rect(Screen.width - 300,0,700,Screen.height), Resources.Load(background[0]) as Texture2D);
+			GUI.DrawTexture(Rect(45,50,220,180), Resources.Load(molecule[3]) as Texture2D);
+			createElement(positionDefenseX, positionDefenseY, defensePos, 0, "defense");
 		GUI.EndGroup();
 	};
 	
 	var force = function(){
-		var positionForceX = [120,166,166,240,240,240,166,75,120]; 
-		var positionForceY = [115,95,29,70,70,190,230,200,140];
+		var positionForceX = [45,95,145,195,225,255];
+		var positionForceY = [133,133,133,133,105,133];
+		
 		GUI.BeginGroup(Rect(Screen.width - 300,0,700,Screen.height), Resources.Load(background[1]) as Texture2D);
-			GUI.DrawTexture(Rect(90,50,150,172), Resources.Load(molecule[0]) as Texture2D);
-			// call the createElement method
-			createElement(positionForceX, positionForceY);
+			GUI.DrawTexture(Rect(45,50,220,180), Resources.Load(molecule[0]) as Texture2D);
+			createElement(positionForceX, positionForceY, forcePos, 1, "force");
 		GUI.EndGroup();
 	};
 	
 	var vision = function(){
-		GUI.BeginGroup(Rect(Screen.width - 300,height / 8,400,450), Resources.Load(background[2]) as Texture2D);
+		var positionVisionX = [45,92,157,195,225,255];
+		var positionVisionY = [137,165,107,133,133,107];
+		
+		GUI.BeginGroup(Rect(Screen.width - 300,0,700,Screen.height), Resources.Load(background[2]) as Texture2D);
+			GUI.DrawTexture(Rect(45,50,220,180), Resources.Load(molecule[1]) as Texture2D);
+			createElement(positionVisionX, positionVisionY, visionPos, 2, "vision");
 		GUI.EndGroup();
 	};
 	
 	var vitesse = function(){
-		GUI.BeginGroup(Rect(Screen.width - 300,height / 8,400,450), Resources.Load(background[3]) as Texture2D);
+		var positionVitesseX = [45,92,170,210,252,210];
+		var positionVitesseY = [130,130,130,105,133,155];
+		
+		GUI.BeginGroup(Rect(Screen.width - 300,0,700,Screen.height), Resources.Load(background[3]) as Texture2D);
+			GUI.DrawTexture(Rect(45,50,220,180), Resources.Load(molecule[2]) as Texture2D);
+			createElement(positionVitesseX, positionVitesseY, vitessePos, 3, "vitesse");
 		GUI.EndGroup();
 	};
 	
@@ -90,17 +127,13 @@ function OnGUI(){
 	
 	// this method create the left side button.
 	if(zoom){
-		Debug.Log(i);
+//		Debug.Log(i);
 		GUI.BeginGroup(Rect(5,height / 5,147,height));
 			for(i = 0; i < bouton.length; i++){
 				if(GUI.Button(Rect(5, offsetHeight, 70, 70), Resources.Load(bouton[i]) as Texture2D, customGuiStyle)){
 					setID(i);
 					PlayerPrefs.SetInt("tabvalue", i);
 					PlayerPrefs.SetInt("EachFrame", 1);
-					/*if(PlayerPrefs.GetInt("steps") < 4){
-						PlayerPrefs.SetInt("steps", 1);
-					}*/
-					
 					step(1);
 				}
 				count++;
@@ -113,9 +146,8 @@ function OnGUI(){
 	// activate the gui element each frame.
 	if(EachFrame){
 		setID(PlayerPrefs.GetInt("tabvalue"));
-		DisplayText(texte, PlayerPrefs.GetInt("texteValue"));
+		DisplayText(texte, PlayerPrefs.GetInt("texteValue"), PlayerPrefs.GetString("ptyName"), PlayerPrefs.GetInt("ptyState"));
 	}
-	
 }
 
 function step(step : int){
@@ -124,6 +156,34 @@ function step(step : int){
 	}
 }
 
+function triggerAnim(){
+	var BasSprite = GameObject.Find("Bas");
+	var BasSpriteComponent = BasSprite.GetComponent(Animator);
+
+		BasSpriteComponent.enabled = true;
+		BasSpriteComponent.runtimeAnimatorController = Resources.Load("foo") as RuntimeAnimatorController;
+
+		yield WaitForSeconds(5);
+		
+		BasSpriteComponent.runtimeAnimatorController = Resources.Load("Bas 2") as RuntimeAnimatorController;
+		yield WaitForSeconds(0.1);
+		BasSpriteComponent.enabled = false;
+}
+
+function sendData(ameliorationID : int, niveau : int){
+	if(ameliorationID == 1){
+		// defense
+	}
+	else if(ameliorationID == 2){
+		// force
+	}
+	else if(ameliorationID == 3){
+		// vision
+	}
+	else{
+		// vitesse
+	}
+}
 
 // zommer la camera sur spike, desactiver le speed
 
@@ -139,9 +199,6 @@ function Update () {
 			timer += Time.deltaTime;
 			camera.GetComponent(Camera).orthographicSize = Mathf.Lerp(1.9, 0.67, timer * 0.8);
 			PlayerPrefs.SetInt("activationSpike", 2);
-			
-			// provisoire
-			
 			PlayerPrefs.SetInt("setPosition", 1);
 		}
 		else{
@@ -155,8 +212,12 @@ function Update () {
 			PlayerPrefs.SetInt("setPosition", 0);
 			PlayerPrefs.SetInt("EachFrame", 0);
 			
-			if(PlayerPrefs.GetInt("steps") == 3){
+			if(PlayerPrefs.GetInt("steps") >= 3){
 				PlayerPrefs.SetInt("steps", 4);
+			}
+			else{
+				//Debug.Log("passe ici");
+				PlayerPrefs.SetInt("steps", 0);
 			}
 		}
 	}
@@ -169,5 +230,8 @@ function Update () {
 			timer = 0;
 		}
 	}
+}
+
+class defense {
 	
 }
